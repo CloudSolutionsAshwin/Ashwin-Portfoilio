96848d0d96669d1269605e855a5e1d0d5f77aa47
Keshav Kant Gupta <iamashwin073@gmail.com>
2023-10-16 19:09:15 +0530

    Blog

---
+++ assets/js/google-maps.js
@@ -0,0 +1,15 @@
+function initMap() {
+    // Latitude and Longitude
+    var myLatLng = {lat: 23.0225, lng: 72.5714};
+
+    var map = new google.maps.Map(document.getElementById('google-maps'), {
+        zoom: 17,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Ahmedabad, India' // Title Location
+    });
+}
